ffbbc6539859ccad1d233013cffecca6036590df
Hamid Mosalla <Xellarix@gmail.com>
2018-03-28 11:47:37 +0430

    add require attribute for value types

---
+++ AspNetCoreWebApiSamples/Helpers/RequireNonDefaultAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebApiSamples.Helpers
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class RequireNonDefaultAttribute : ValidationAttribute
+    {
+        public RequireNonDefaultAttribute() : base("The {0} field requires a non-default value.") { }
+
+        public override bool IsValid(object value)
+        {
+            return value != null && !Equals(value, Activator.CreateInstance(value.GetType()));
+        }
+    }
+}
