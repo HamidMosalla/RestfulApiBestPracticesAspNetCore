f073cfaeef5a068898a77b4a7eb05a0b6b28e358
Hamid Mosalla <Xellarix@gmail.com>
2018-05-10 17:25:57 +0430

    add api client project

---
+++ ApiClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApiClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
